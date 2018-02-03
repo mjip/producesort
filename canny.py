2a903e5f6b732b3e06f3a6e5b3e5430f55749f8c
jidicula <johanan.idicula@gmail.com>
2018-02-03 18:24:42 -0500

    module for edge detection and background subtraction

--- canny.py
+++ canny.py
@@ -1,58 +1,53 @@
-import numpy as np
-import matplotlib.pyplot as plt
 from scipy import ndimage as ndi
 
-from skimage import io, feature, filters, data, color
+from skimage import io, feature
 from skimage.color import rgb2gray
 from scipy import ndimage
 from copy import deepcopy
 
 
-# Generate noisy image of a square
-#im = np.zeros((128, 128))
-#im[32:-32, 32:-32] = 1
+def edge(filename):
+        im = io.imread(filename)
+        im_copy = deepcopy(im)
 
-#im = ndi.rotate(im, 15, mode='constant')
-#im = ndi.gaussian_filter(im, 4)
-#im += 0.2 * np.random.random(im.shape)
+        # Zeroing red channel, combined blue + green channels into
+        # single grayscale channel.
+        im[:, :, 0] = 0
+        im_grey = ndimage.gaussian_filter(rgb2gray(im), 2)
 
-im = io.imread('img/good/top.jpg')
-im2 = deepcopy(im)
+        # Compute the Canny filter for two values of sigma
+        # image must be 2d
 
-#got rid of red, combined blue + green
-im[:,:,0] = 0
-im_grey = rgb2gray(im)
-im_grey = ndimage.gaussian_filter(im_grey, 2)
+        edges = feature.canny(im_grey, sigma=3)
 
-# Compute the Canny filter for two values of sigma
-# image must be 2d
+        mask = ndi.binary_fill_holes(edges)
 
-edges2 = feature.canny(im_grey, sigma=3)
-
-mask2 = ndi.binary_fill_holes(edges2)
-
-im_edges2 = deepcopy(im2)
-#for c in range(3):
-	#im_edges2 = im2[:,:,c] * mask2[:,:,0]
-for x in range(mask2.shape[0]):
-	for y in range(mask2.shape[1]):
-		if (mask2[x,y] == 0):
-			im_edges2[x,y] = im[x,y] * mask2[x,y]
+        im_edges = im_copy
+        for x in range(mask.shape[0]):
+                for y in range(mask.shape[1]):
+                        if (mask[x, y] == 0):
+                                im_edges[x, y] = im[x, y] * mask[x, y]
+        return mask, im_edges
 
 
-# display results
-fig, (ax1, ax3) = plt.subplots(nrows=1, ncols=2, figsize=(8, 3),
-                                    sharex=True, sharey=True)
+# Uncomment below lines to test output
 
-ax1.imshow(im2, cmap=plt.cm.gray)
-ax1.axis('off')
-ax1.set_title('noisy image', fontsize=20)
+# import matplotlib.pyplot as plt
+
+# mask, im_edges = edge("img/good/top.jpg")
+
+# fig, (ax1, ax3) = plt.subplots(nrows=1, ncols=2, figsize=(8, 3),
+#                                     sharex=True, sharey=True)
+
+# ax1.imshow(mask, cmap=plt.cm.gray)
+# ax1.axis('off')
+# ax1.set_title('mask', fontsize=20)
 
 
-ax3.imshow(im_edges2, cmap=plt.cm.gray)
-ax3.axis('off')
-ax3.set_title('Canny filter, $\sigma=3$', fontsize=20)
+# ax3.imshow(im_edges, cmap=plt.cm.gray)
+# ax3.axis('off')
+# ax3.set_title('No background, $\sigma=3$', fontsize=20)
 
-fig.tight_layout()
+# fig.tight_layout()
 
-plt.show()
+# plt.show()
